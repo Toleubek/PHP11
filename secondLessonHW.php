671a770865b67b418ab1327e5701bb9b880a1539
Toleubek <m.toleubek@mail.ru>
2017-04-21 10:04:52 +0600

    Update secondLessonHW.php

--- secondLessonHW.php
+++ secondLessonHW.php
@@ -1,28 +1,34 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Second Home work</title>
-	<meta charset="UTF-8">
-</head>
-<body>
-<?php
-$x = rand(0, 100);
-echo "Число - ".$x.'<br>';
-$p1 = 1;
-$p2 = 1;
+	<head>
+		<title>Second Home work</title>
+		<meta charset="UTF-8">	
+	</head>
+	<body>
+		<?php
+		error_reporting(E_ALL);
 
-
-if($p1 > $x){
-	echo 'Задуманное число не входит в числовой ряд';		
-}elseif($p1 === $x){
-	echo "Задуманное число входит в числовой ряд";
-}else{
-	$p3 = $p1; 
-	$p1 = $p1 + $p2; 
-	$p2 = $p3;		
-}	
-?>
-</body>
+		$x = rand(0, 100);
+		echo 'Число - '.$x.'<br>';
+		//$p1 = 1;
+		//$p2 = 1;
+		
+		for ($p1 = 1;$p2 = 1;){
+			if($p1>$x){
+				echo 'Задуманное число не входит в числовой ряд';
+				break;		
+			}else if($p1 === $x){
+				echo 'Задуманное число входит в числовой ряд';
+				break;
+			}else{
+				$p3 = $p1; 
+				$p1 = $p1 + $p2; 
+				$p2 = $p3;		
+			}
+		}	
+		?>
+		
+	</body>
 </html>
 
 
