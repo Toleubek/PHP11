d2d76b0eff1967e7dacf1db4067f8780ed60005d
Toleubek <m.toleubek@mail.ru>
2017-05-02 20:38:53 +0600

    Create homeWork6.php

---
+++ homeWork6.php
@@ -0,0 +1,8 @@
+<meta charset="utf-8">
+<form action="response.php" method="post" enctype="multipart/form-data">
+   <p><b>Форма загрузки результатов теста на сервер</b></p>
+   <p><b>First name: </b><input type="text" name="firstName"></p>
+   <p><b>Second name: </b><input type="text" name="secondName"></p>
+   <p><b>Выберите файл для загрузки: <input type="file" name="uploaded_file"></b></p>
+   <p><input type="submit" value="Отправить"></p>
+</form> 
